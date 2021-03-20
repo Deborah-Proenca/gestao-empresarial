251b04fa603dd2e6cb9a03dcfeaae78df104bf3c
Deborah Proenca <deborah.proen@gmail.com>
2021-03-20 15:34:39 -0300

    Adiciona configs

---
+++ src/test/java/com/gestaoempresarial/gestaoempresarial/GestaoEmpresarialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestaoempresarial.gestaoempresarial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoEmpresarialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
